dbea3898db937b04470b66480a8bb41048d79a19
TEMESGENTIKABO <temehyaway@gmail.com>
2024-11-06 21:22:36 +0800

    3rd commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,10 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import AppNavigation from './navigation/Navigation';
+
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen';
-import SearchScreen from './screens/SearchScreen';
-import DetailsScreen from './screens/DetailsScreen'
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Search" component={SearchScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AppNavigation />;
 }
